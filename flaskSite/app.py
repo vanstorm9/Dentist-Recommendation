cb7a1839ecbba0cf15a662985ce75b4108ca5402
Vanstorm9 <antlowhur@yahoo.com>
2019-05-31 00:18:06 -0400

    Adding stuff

--- flaskSite/app.py
+++ flaskSite/app.py
@@ -18,23 +18,14 @@ def dentistRecommend():
     topNRankNum = 5
     splitNum = 300
     contractDir = './company0/'
-    
+
     if request.method == 'POST':
-        result = request.form
-        if result['top'].isdigit():
-            topNRankNum = int(result['top'])
-        if result['contractType'] == 'gym':
-            splitNum = 300
-            contractDir = './company0/'
-        elif result['contractType'] == 'dating':
-            splitNum = 400
-            contractDir = './dating/'
-        else:
-            # We are doing hotel
-            splitNum = 400
-            contractDir = './hotel/'
-    else:
-        topNRankNum  = request.args.get('top',None)
+        #searchQuery = request.args.get('search',None)
+        searchQuery = request.form['search']
+        print(searchQuery)
+    
+
+    topNRankNum  = request.args.get('top',None)
 
     if topNRankNum is None:
         topNRankNum = 5
